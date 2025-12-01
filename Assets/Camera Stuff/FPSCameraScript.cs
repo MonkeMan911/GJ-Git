9e3aa430d6918d58711dfb3f2c2833886fcee097
Monke <752535@newcollege.ac.uk>
2025-12-01 22:29:26 +0000

    edited code for arrows

--- Assets/Camera Stuff/FPSCameraScript.cs
+++ Assets/Camera Stuff/FPSCameraScript.cs
@@ -7,16 +7,16 @@ public class FPSCameraScript : MonoBehaviour
     public Transform player;
     public float mouseSensitivity = 2f;
     float camerVerticalRotation = 0f;
-    bool lockedCursor = true;
+
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Toggle cursor lock/unlock with LeftControl
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             if (Cursor.lockState == CursorLockMode.Locked)
@@ -31,13 +31,17 @@ public class FPSCameraScript : MonoBehaviour
             }
         }
 
-        float inputX = Input.GetAxis("Mouse X")*mouseSensitivity;
-        float inputY = Input.GetAxis("Mouse Y")*mouseSensitivity;
+        // Only rotate camera when cursor is locked
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+            float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        camerVerticalRotation -= inputY;
-        camerVerticalRotation = Mathf.Clamp(camerVerticalRotation, -90f, 90f);
-        transform.localEulerAngles = Vector3.right * camerVerticalRotation;
+            camerVerticalRotation -= inputY;
+            camerVerticalRotation = Mathf.Clamp(camerVerticalRotation, -90f, 90f);
+            transform.localEulerAngles = Vector3.right * camerVerticalRotation;
 
-        player.Rotate(Vector3.up * inputX);
+            player.Rotate(Vector3.up * inputX);
+        }
     }
 }
