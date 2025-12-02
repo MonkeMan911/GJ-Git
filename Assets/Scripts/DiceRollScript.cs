9b86538b41c6f9b1b5380e3030f024c612011be6
Monke <752535@newcollege.ac.uk>
2025-12-02 12:22:40 +0000

    Created Die

--- Assets/Scripts/DiceRollScript.cs
+++ Assets/Scripts/DiceRollScript.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiceRollScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text diceOutcomeText;
+    public int diceOutcomeNumber;
     void Start()
     {
-        
+        diceOutcomeNumber = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void DiceRoll() 
+    { 
+        diceOutcomeNumber = Random.Range(1, 6);
+        diceOutcomeText.text = diceOutcomeNumber.ToString();
     }
 }
